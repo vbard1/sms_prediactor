d331920cf6893f3a0b10e70760af6af1a8cd4729
Victor BARDIN <victor.bardin@insa-lyon.fr>
2024-02-10 03:45:15 +0100

    méthode des cribles <3

--- src/model_builder/ModelTrainer.java
+++ src/model_builder/ModelTrainer.java
@@ -30,7 +30,8 @@ public class ModelTrainer {
       Main.log.info(word.toString());
     }
     LogFactory.toggleConsoleLogs(true, Main.log);
-    // TODO nettoyer
+    // TODO tenter de varier la taille du crible et le paramétriser
+    // TODO arrondir les stats (moins de volume?)
     // TODO Mettre tout l'arbre dans un fichier ou une structure de données appropriée (?)
     // TODO développer la factory de suggestion live (android? -> cycle de vie : en conf introduire la période à considérer depuis le jour J (backtime eg. -3 mois)
     // TODO implémenter un runner de réentrainnement de model réguler)
@@ -38,33 +39,59 @@ public class ModelTrainer {
     // TODO ...et dans ce cas lister les fichiers existant (pour celà, y associer des données notamment le destinataire concerné)
     // TODO la première fenêtre devrait être le paramétrage du fichier de config (Config.write?)
     // TODO améliroer le découpage des phrases et des messages (une nouvelle phrase c'est smiley/.../.)
+    // TODO TOCONSIDER passer en crible de n mots pour éviter les effets niche? ajouter pour celà une sentence par n mots décalés sur le tableau de la sentence
+    // exemple :
+    // mot mot mot [ mot mot mot ]
+    // mot mot [ mot mot mot ] mot
+    // mot [ mot mot mot ] mot mot
+    // [ mot mot mot ] mot mot mot
+    // ICI CHAQUE CRIBLE DE 3 MOTS EST UNE SENTENCE (paramétrer)
     // TOCONSIDER adapter la réponse en fonction du message reçu? (?)
   }
 
   public static void traverseNodes(Node node) {
-    // Vérifier si le nœud actuel est un élément (et non un texte, un commentaire,
-    // etc.)
     if (node.getNodeType() == Node.ELEMENT_NODE) {
       Element element = (Element) node;
 
-      // Traiter l'élément selon ses besoins
       if (element.getNodeName().equals("sms")) {
         String messageBody = element
           .getAttributeNode("body")
           .getValue()
           .toString();
 
-        String emojiRegexBlock="(?:[\uD83C\uDF00-\uD83D\uDDFF]|[\uD83E\uDD00-\uD83E\uDDFF]|[\uD83D\uDE00-\uD83D\uDE4F]|[\uD83D\uDE80-\uD83D\uDEFF]|[\u2600-\u26FF]\uFE0F?|[\u2700-\u27BF]\uFE0F?|\u24C2\uFE0F?|[\uD83C\uDDE6-\uD83C\uDDFF]{1,2}|[\uD83C\uDD70\uD83C\uDD71\uD83C\uDD7E\uD83C\uDD7F\uD83C\uDD8E\uD83C\uDD91-\uD83C\uDD9A]\uFE0F?|[\u0023\u002A\u0030-\u0039]\uFE0F?\u20E3|[\u2194-\u2199\u21A9-\u21AA]\uFE0F?|[\u2B05-\u2B07\u2B1B\u2B1C\u2B50\u2B55]\uFE0F?|[\u2934\u2935]\uFE0F?|[\u3030\u303D]\uFE0F?|[\u3297\u3299]\uFE0F?|[\uD83C\uDE01\uD83C\uDE02\uD83C\uDE1A\uD83C\uDE2F\uD83C\uDE32-\uD83C\uDE3A\uD83C\uDE50\uD83C\uDE51]\uFE0F?|[\u203C\u2049]\uFE0F?|[\u25AA\u25AB\u25B6\u25C0\u25FB-\u25FE]\uFE0F?|[\u00A9\u00AE]\uFE0F?|[\u2122\u2139]\uFE0F?|\uD83C\uDC04\uFE0F?|\uD83C\uDCCF\uFE0F?|[\u231A\u231B\u2328\u23CF\u23E9-\u23F3\u23F8-\u23FA]\uFE0F?)";
-        String emojiRegexBolck2="\\p{Block=Emoticons}";
+        String emojiRegexBlock =
+          "(?:[\uD83C\uDF00-\uD83D\uDDFF]|[\uD83E\uDD00-\uD83E\uDDFF]|[\uD83D\uDE00-\uD83D\uDE4F]|[\uD83D\uDE80-\uD83D\uDEFF]|[\u2600-\u26FF]\uFE0F?|[\u2700-\u27BF]\uFE0F?|\u24C2\uFE0F?|[\uD83C\uDDE6-\uD83C\uDDFF]{1,2}|[\uD83C\uDD70\uD83C\uDD71\uD83C\uDD7E\uD83C\uDD7F\uD83C\uDD8E\uD83C\uDD91-\uD83C\uDD9A]\uFE0F?|[\u0023\u002A\u0030-\u0039]\uFE0F?\u20E3|[\u2194-\u2199\u21A9-\u21AA]\uFE0F?|[\u2B05-\u2B07\u2B1B\u2B1C\u2B50\u2B55]\uFE0F?|[\u2934\u2935]\uFE0F?|[\u3030\u303D]\uFE0F?|[\u3297\u3299]\uFE0F?|[\uD83C\uDE01\uD83C\uDE02\uD83C\uDE1A\uD83C\uDE2F\uD83C\uDE32-\uD83C\uDE3A\uD83C\uDE50\uD83C\uDE51]\uFE0F?|[\u203C\u2049]\uFE0F?|[\u25AA\u25AB\u25B6\u25C0\u25FB-\u25FE]\uFE0F?|[\u00A9\u00AE]\uFE0F?|[\u2122\u2139]\uFE0F?|\uD83C\uDC04\uFE0F?|\uD83C\uDCCF\uFE0F?|[\u231A\u231B\u2328\u23CF\u23E9-\u23F3\u23F8-\u23FA]\uFE0F?)";
         List<String> sentences = Arrays
-          .asList(messageBody.split("\\.+|…|!+|"+emojiRegexBlock))
+          .asList(messageBody.split("\\.+|…|!+|" + emojiRegexBlock))
           .stream()
           .map(String::trim)
           .filter(sentence -> !sentence.isEmpty())
           .collect(Collectors.toList());
 
         for (String sentence : sentences) {
-          addSentence(sentence.trim().split(" "));
+          /** //_v1 -> réduit le volume de données mais fait des niches (avec des mots spéciaux)
+           * addSentence(sentence.trim().split(" "));
+           */
+          // _v2 -> fonctionne très bien pour éliminer les niches mais démultiplie le nombre de données : le faire à la manip? (un enfer de temps de calcul)
+          //-> TODO tenter de varier la taille du crible et le paramétriser
+          int n = 5; // taille du crible
+
+          String[] mots = sentence.trim().split(" ");
+          int longueur = mots.length;
+          for (int i = 0; i < longueur; i++) {
+            StringBuilder nouvellePhrase = new StringBuilder();
+
+            // Ajouter chaque mot à la nouvelle phrase
+            for (int j = i; j < longueur; j++) {
+              nouvellePhrase.append(mots[j]).append(" ");
+
+              // Vérifier si nous avons atteint la taille du groupe de mots
+              if ((j - i + 1) % n == 0) {
+                addSentence(nouvellePhrase.toString().trim().split(" "));
+                nouvellePhrase = new StringBuilder();
+              }
+            }
+          }
         }
       }
       NodeList childrenNodes = element.getChildNodes();
