40ee317b2a12e4bb01c3b15fa3df2d655ecb8249
Victor BARDIN <victor.bardin@insa-lyon.fr>
2024-02-10 02:37:43 +0100

    Improved sentence detection

--- src/model_builder/ModelTrainer.java
+++ src/model_builder/ModelTrainer.java
@@ -2,6 +2,8 @@ package src.model_builder;
 
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.w3c.dom.*;
 import src.Main;
 import src.log_factory.LogFactory;
@@ -17,7 +19,7 @@ public class ModelTrainer {
   ) {
     Main.log.info("Starting Model Trainer with address " + selectedAddress);
     //computing sentences into the occurence tree
-    wordTree = new WordTree("ROOT_OCCURENCES",0);
+    wordTree = new WordTree("ROOT_OCCURENCES", 0);
     traverseNodes(preparedXMLSources.getDocumentElement());
 
     //computing frequencies out of occurence tree
@@ -28,16 +30,14 @@ public class ModelTrainer {
       Main.log.info(word.toString());
     }
     LogFactory.toggleConsoleLogs(true, Main.log);
-
     // TODO nettoyer
     // TODO Mettre tout l'arbre dans un fichier ou une structure de données appropriée (?)
-    // TODO Filtrer par message reçu ou envoyé
     // TODO développer la factory de suggestion live (android? -> cycle de vie : en conf introduire la période à considérer depuis le jour J (backtime eg. -3 mois)
-    // et implémenter un runner de réentrainnement de model réguler)
+    // TODO implémenter un runner de réentrainnement de model réguler)
     // TODO implémenter l'option non ne pas réentrainner le modèle...
-    // TODO paramétrer l'indépendance de la casse
-    // TODO améliroer le découpage des phrases et des messages (une nouvelle phrase c'est smiley/.../.)
     // TODO ...et dans ce cas lister les fichiers existant (pour celà, y associer des données notamment le destinataire concerné)
+    // TODO la première fenêtre devrait être le paramétrage du fichier de config (Config.write?)
+    // TODO améliroer le découpage des phrases et des messages (une nouvelle phrase c'est smiley/.../.)
     // TOCONSIDER adapter la réponse en fonction du message reçu? (?)
   }
 
@@ -53,9 +53,19 @@ public class ModelTrainer {
           .getAttributeNode("body")
           .getValue()
           .toString();
-        // TODO découper par séparateusr (points smileyx) et boucler la suite sur le
-        // tableau obtenu
-        addSentence(messageBody.split(" "));
+
+        String emojiRegexBlock="(?:[\uD83C\uDF00-\uD83D\uDDFF]|[\uD83E\uDD00-\uD83E\uDDFF]|[\uD83D\uDE00-\uD83D\uDE4F]|[\uD83D\uDE80-\uD83D\uDEFF]|[\u2600-\u26FF]\uFE0F?|[\u2700-\u27BF]\uFE0F?|\u24C2\uFE0F?|[\uD83C\uDDE6-\uD83C\uDDFF]{1,2}|[\uD83C\uDD70\uD83C\uDD71\uD83C\uDD7E\uD83C\uDD7F\uD83C\uDD8E\uD83C\uDD91-\uD83C\uDD9A]\uFE0F?|[\u0023\u002A\u0030-\u0039]\uFE0F?\u20E3|[\u2194-\u2199\u21A9-\u21AA]\uFE0F?|[\u2B05-\u2B07\u2B1B\u2B1C\u2B50\u2B55]\uFE0F?|[\u2934\u2935]\uFE0F?|[\u3030\u303D]\uFE0F?|[\u3297\u3299]\uFE0F?|[\uD83C\uDE01\uD83C\uDE02\uD83C\uDE1A\uD83C\uDE2F\uD83C\uDE32-\uD83C\uDE3A\uD83C\uDE50\uD83C\uDE51]\uFE0F?|[\u203C\u2049]\uFE0F?|[\u25AA\u25AB\u25B6\u25C0\u25FB-\u25FE]\uFE0F?|[\u00A9\u00AE]\uFE0F?|[\u2122\u2139]\uFE0F?|\uD83C\uDC04\uFE0F?|\uD83C\uDCCF\uFE0F?|[\u231A\u231B\u2328\u23CF\u23E9-\u23F3\u23F8-\u23FA]\uFE0F?)";
+        String emojiRegexBolck2="\\p{Block=Emoticons}";
+        List<String> sentences = Arrays
+          .asList(messageBody.split("\\.{1,3}|…|"+emojiRegexBlock))
+          .stream()
+          .map(String::trim)
+          .filter(sentence -> !sentence.isEmpty())
+          .collect(Collectors.toList());
+
+        for (String sentence : sentences) {
+          addSentence(sentence.trim().split(" "));
+        }
       }
       NodeList childrenNodes = element.getChildNodes();
       for (int i = 0; i < childrenNodes.getLength(); i++) {
@@ -75,9 +85,13 @@ public class ModelTrainer {
 
     //traitement ittératif des mots
     String currentWordString;
-    for (int currentWordIndex = 0; currentWordIndex < sentenceList.size();currentWordIndex++) {
+    for (
+      int currentWordIndex = 0;
+      currentWordIndex < sentenceList.size();
+      currentWordIndex++
+    ) {
       currentWordString = sentenceList.get(currentWordIndex);
-      word = new WordTree(currentWordString,referenceWord.depth);
+      word = new WordTree(currentWordString, referenceWord.depth);
 
       // 1. récupérer l'index du mot de la phrase s'il existe dans les mots suivants et incrémenter
       found = false;
